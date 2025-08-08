bf7d97d6b1ffb0ca1ce1b95cab3239d7f98c5f4a
Willian Pinho <willianpinho@gmail.com>
2025-08-08 12:39:00 -0300

    Revert "feat(android): Add production-ready Android implementation with Clean Architecture"
    This reverts commit c24e911030be352a69a29b0091124dd0d52aecd5.

--- dynamic-forms-android/features/formlist/src/main/java/com/example/dynamicforms/features/formlist/FormListEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.example.dynamicforms.features.formlist
-
-sealed interface FormListEvent {
-    data object LoadForms : FormListEvent
-    data object Retry : FormListEvent
-    data class NavigateToFormEntries(val formId: String) : FormListEvent
-}
